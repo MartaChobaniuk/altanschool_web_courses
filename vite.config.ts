5521c3ad89e579e20ed2548e8566305ad859c6d6
Marta Chobaniuk <marta.chobaniuk@gmail.com>
2025-05-20 14:58:26 +0300

    Add GitHub Pages deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/altanschool_web_courses/',
   plugins: [react()],
+  server: {
+    open: true,
+  },
 })
