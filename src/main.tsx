309a3670b653a48d791335e69174fe4261ffb774
Marta Chobaniuk <marta.chobaniuk@gmail.com>
2025-05-22 17:04:25 +0300

    added Readme file

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import { store } from './store/index.ts'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/altanschool_web_courses/">
+      <BrowserRouter basename="/">
         <Root />
       </BrowserRouter>
     </Provider>
